e1addabef73b8d0ebf389b17ba8b8ed668765cb5
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 22:31:25 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ByronCalderon3A_Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie_numero serie_numero= new Serie_numero();
+            serie_numero.Media_aritmetica();
+        }
+    }
+}
